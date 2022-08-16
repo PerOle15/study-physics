9f38937a4f7bca33bfb776987e9e82e1dec298f2
PerOle15 <ole.pechmann@gmail.com>
2022-08-16 16:15:51 +0200

    LaTeX working

--- src/studyPages/InclinedPlanePage.jsx
+++ src/studyPages/InclinedPlanePage.jsx
@@ -1,4 +1,5 @@
 import InclinedPlaneSketch from '../sketches/InclinedPlaneSketch'
+import Latex from 'react-latex'
 
 function InclinedPlanePage() {
   document.title = 'Fysihka - Schiefe Ebene'
@@ -6,6 +7,38 @@ function InclinedPlanePage() {
     <div className='container'>
       <p className='page-heading'>Schiefe Ebene</p>
       <InclinedPlaneSketch />
+      <section className='topic-description'>
+        <div className='short-description'>
+          <p>
+            Eine schiefe Ebene ist eine einfache Maschine, die das Heben oder
+            Bewegen von Gegenständen erleichtert. Der Grundgedanke einer
+            schiefen Ebene besteht darin, dass das zu bewegende Objekt entlang
+            einer Fläche bewegt wird, die nicht gerade nach oben, sondern schräg
+            ist. Diese schräge Fläche kann das Bewegen des Objekts erleichtern,
+            weil man die Schwerkraft zu seinem Vorteil nutzt.
+          </p>
+        </div>
+        <div className='important-formulas'>
+          <p className='section-title'>
+            Die wichtigsten Formeln auf einen Blick:
+          </p>
+          <Latex>$3\times 4$</Latex>
+        </div>
+        <br />
+        <div className='long-description'>
+          <p>
+            Mit einer schiefen Ebene kann man keine Energie einsparen. Was an
+            Kraft gespart wird, muss in Form des Weges ergänzt werden («Goldene
+            Regel der Mechanik»). Ein Körper, der auf einer schiefen Ebene
+            steht, wird durch seine Gewichtskraft bergab beschleunigt. Die
+            Gewichtskraft wird in zwei Teile zerlegt: die Kraft senkrecht zur
+            schiefen Ebene (Normalkraft) und die Kraft parallel zur schiefen
+            Ebene (Hangabtriebskraft). Wenn die Hangabtriebskraft (FH) größer
+            ist als die Reibungskraft zwischen dem Körper und der schiefen
+            Ebene, rutscht der Körper.
+          </p>
+        </div>
+      </section>
     </div>
   )
 }
--- src/studyPages/PendulumPage.jsx
+++ src/studyPages/PendulumPage.jsx
@@ -1,10 +1,47 @@
 import PendulumSketch from '../sketches/PendulumSketch'
+import Latex from 'react-latex'
 
 function PendulumPage() {
+  const oscillationPeriod = '$$T=2\\pi\\sqrt{\\frac{l}{g}} $$'
   return (
     <div className='container'>
       <p className='page-heading'>Pendel</p>
       <PendulumSketch />
+      <section className='topic-description'>
+        <div className='short-description'>
+          <p>
+            Ein Pendel ist eines der einfachsten physikalischen Objekte.
+            Grundsätzlich versteht man unter einem Pendel einen Körper, der
+            drehbar aufgehängt ist. Die einfachste Art eines solchen Pendels ist
+            ein Fadenpendel, also ein Körper, der mithilfe eines Fadens
+            aufgehängt ist. Die Einfachheit eines Pendels kommt daher, dass
+            dessen Schwingungsdauer nur von der Länge der Schnur und der
+            Gravitation abhängt.
+          </p>
+        </div>
+        <div className='important-formulas'>
+          <p className='section-title'>
+            Die wichtigsten Formeln auf einen Blick:
+          </p>
+          <p>
+            Pendelschwingungsdauer: <Latex>{oscillationPeriod}</Latex>
+          </p>
+        </div>
+        <br />
+        <div className='long-description'>
+          <p>
+            Mit einer schiefen Ebene kann man keine Energie einsparen. Was an
+            Kraft gespart wird, muss in Form des Weges ergänzt werden («Goldene
+            Regel der Mechanik»). Ein Körper, der auf einer schiefen Ebene
+            steht, wird durch seine Gewichtskraft bergab beschleunigt. Die
+            Gewichtskraft wird in zwei Teile zerlegt: die Kraft senkrecht zur
+            schiefen Ebene (Normalkraft) und die Kraft parallel zur schiefen
+            Ebene (Hangabtriebskraft). Wenn die Hangabtriebskraft (FH) größer
+            ist als die Reibungskraft zwischen dem Körper und der schiefen
+            Ebene, rutscht der Körper.
+          </p>
+        </div>
+      </section>
     </div>
   )
 }
