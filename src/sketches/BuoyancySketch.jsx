0a42538143b00ed358977e56b13a056145ca81f8
PerOle15 <ole.pechmann@gmail.com>
2022-10-13 11:41:05 +0200

    buoyancy scaled

--- src/sketches/BuoyancySketch.jsx
+++ src/sketches/BuoyancySketch.jsx
@@ -2,22 +2,22 @@ import Sketch from 'react-p5'
 
 function BuoyancySketch() {
   const frames = 60
-  const scale = 200
-  const localGravity = 9.81
+  const scale = 50
+  const g = 9.81
   const flowResistance = 1.1
 
   let volumeSlider
   let densitySlider
   let liquidDensitySlider
 
-  const minVolume = 0.001
-  const maxVolume = 0.1
-  const startingVolume = 0.05
+  const minVolume = 0.1
+  const maxVolume = 5
+  const startingVolume = 2
   const minDensity = 400
   const maxDensity = 2000
   const startingDensity = 1500
-  const minLiquidDensity = 400
-  const maxLiquidDensity = 2000
+  const minLiquidDensity = minDensity
+  const maxLiquidDensity = maxDensity
   const startingLiquidDensity = 1000
 
   let cube
@@ -49,6 +49,7 @@ function BuoyancySketch() {
         volumeLabel.html(
           `Volumen Würfel: ${volumeSlider.value().toFixed(3)} m<sup>3</sup>`
         )
+        cube.updateVolume()
       })
     volumeLabel.html(
       `Volumen Würfel: ${volumeSlider.value().toFixed(3)} m<sup>3</sup>`
@@ -67,6 +68,7 @@ function BuoyancySketch() {
         densityLabel.html(
           `Dichte Würfel: ${Math.floor(densitySlider.value())} kg/m<sup>3</sup>`
         )
+        cube.updateDensity()
       })
     densityLabel.html(
       `Dichte Würfel: ${Math.floor(densitySlider.value())} kg/m<sup>3</sup>`
@@ -92,6 +94,7 @@ function BuoyancySketch() {
             liquidDensitySlider.value()
           )} kg/m<sup>3</sup>`
         )
+        liquid.density = liquidDensitySlider.value()
       })
     liquidDensityLabel.html(
       `Dichte Flüssigkeit: ${Math.floor(
@@ -118,36 +121,37 @@ function BuoyancySketch() {
   class Cube {
     constructor(p) {
       this.p = p
-      this.density = 1500
-      this.volume = 0.05
-      this.dimensions = Math.cbrt(this.volume)
-      this.displayDimensions = this.dimensions * scale
+      this.width = this.p.width / scale
+      this.height = this.p.height / scale
+      this.density = startingDensity
+      this.volume = startingVolume
+      this.dim = Math.cbrt(this.volume)
       this.mass = this.density * this.volume
-      this.x = this.p.width / 2 - this.displayDimensions / 2
-      this.y = 200
+      this.x = this.width / 2 - this.dim / 2
+      this.y = 200 / scale
       this.offsetX = 0
       this.offsetY = 0
-      this.velocity = this.p.createVector(0, 0)
-      this.acc = this.p.createVector(0, 0)
+      this.vel = 0
+      this.acc = 0
       this.submergedVolume = 0
       this.submergedPart = 0
       this.dragging = false
     }
 
     pressed() {
-      const px = this.p.mouseX
-      const py = this.p.mouseY
+      const px = this.p.mouseX / scale
+      const py = this.p.mouseY / scale
       if (
         px > this.x &&
-        px < this.x + this.displayDimensions &&
+        px < this.x + this.dim &&
         py > this.y &&
-        py < this.y + this.displayDimensions
+        py < this.y + this.dim
       ) {
         this.dragging = true
         this.offsetX = this.x - px
         this.offsetY = this.y - py
-        this.acc.y = 0
-        this.velocity.y = 0
+        this.acc = 0
+        this.vel = 0
       }
     }
 
@@ -156,122 +160,116 @@ function BuoyancySketch() {
     }
 
     calcAcceleration() {
-      this.acc.y = (localGravity / frames ** 2) * scale
-      if (this.y + this.displayDimensions) {
+      this.acc = g
+      if (this.y + this.dim) {
         // Fläche durch 2 Teilen, damit die Abbremsung nicht so gross ist
-        const area = this.dimensions ** 2 / 2
+        const area = this.dim ** 2 / 2
         // Formel aus: https://www.leifiphysik.de/mechanik/stroemungslehre/grundwissen/stroemungswiderstand-und-crmw-wert
         const resistanceForce =
-          (1 / 2) * flowResistance * this.density * area * this.velocity.y
-        const resistanceAcc =
-          (resistanceForce / (this.density * this.volume) / frames ** 2) * scale
-        this.acc.y -= resistanceAcc
+          (1 / 2) * flowResistance * this.density * area * this.vel
+        const resistanceAcc = resistanceForce / (this.density * this.volume)
+        this.acc -= resistanceAcc
       }
-      if (this.y + this.displayDimensions > liquid.y) {
+      if (this.y + this.dim > liquid.y) {
         if (this.y > liquid.y) {
           this.submergedPart = 1
           this.submergedVolume = this.volume * this.submergedPart
         } else {
-          this.submergedPart =
-            (this.y + this.displayDimensions - liquid.y) /
-            this.displayDimensions
+          this.submergedPart = (this.y + this.dim - liquid.y) / this.dim
           this.submergedVolume = this.volume * this.submergedPart
         }
-        const buoyancyForce =
-          ((this.submergedVolume * liquid.density * localGravity) /
-            frames ** 2) *
-          scale
+        const buoyancyForce = this.submergedVolume * liquid.density * g
         const buoyancyAcceleration = buoyancyForce / this.mass
-        this.acc.y -= buoyancyAcceleration
+        this.acc -= buoyancyAcceleration
       }
     }
 
-    update() {
-      // Werte von den Slidern an passen
+    updateDensity() {
       this.density = densitySlider.value()
-      this.volume = volumeSlider.value()
-      this.lastDisplayDimensions = this.displayDimensions
-      this.dimensions = Math.cbrt(this.volume)
-      this.displayDimensions = this.dimensions * scale
-      this.x =
-        this.x + (this.lastDisplayDimensions - this.displayDimensions) / 2
       this.mass = this.density * this.volume
+    }
+
+    updateVolume() {
+      this.volume = volumeSlider.value()
+      this.lastdim = this.dim
+      this.dim = Math.cbrt(this.volume)
+      this.x = this.x + (this.lastdim - this.dim) / 2
+      this.mass = this.density * this.volume
+    }
+
+    update() {
       // Überprüfen, ob Würfel über den unteren Rand hinaus geht
-      if (this.y + this.displayDimensions + this.velocity.y < this.p.height) {
+      if (this.y + this.dim + this.vel / frames < this.height) {
         // Überprüfen, ob Würfel über den oberen Rand hinaus geht
-        if (this.y + this.velocity.y < 0) {
-          this.velocity.y *= -1
+        if (this.y + this.vel / frames < 0) {
+          this.vel *= -1
           this.y = 0
         }
         // Beschleunigung des Würfels berechnen
         this.calcAcceleration()
-        this.velocity.y += this.acc.y
+        this.vel += this.acc / frames
         // Wasser-/Luftwiderstand (vereinfacht)
-        // this.velocity.y *= 0.993
-        this.y += this.velocity.y
+        // this.vel *= 0.993
+        this.y += this.vel / frames
       } else {
-        this.velocity.y *= -1
-        this.y = this.p.height - this.displayDimensions
+        this.vel *= -1
+        this.y = this.height - this.dim
       }
     }
 
     display() {
       if (this.dragging) {
-        this.x = this.p.mouseX + this.offsetX
-        this.y = this.p.mouseY + this.offsetY
+        this.x = this.p.mouseX / scale + this.offsetX
+        this.y = this.p.mouseY / scale + this.offsetY
         if (this.x < 0) {
           this.x = 0
-        } else if (this.x + this.displayDimensions > this.p.width) {
-          this.x = this.p.width - this.displayDimensions
+        } else if (this.x + this.dim > this.width) {
+          this.x = this.width - this.dim
         }
         if (this.y < 0) {
           this.y = 0
-        } else if (this.y + this.displayDimensions > this.p.height) {
-          this.y = this.p.height - this.displayDimensions
+        } else if (this.y + this.dim + this.vel / frames > this.height) {
+          this.y = this.height - this.dim
           // Kleiner Abstand, damit der Würfel nicht am Boden stecken bleibt
-          this.y -= 0.0001
+          this.y -= 0.000001
         }
-        this.p.fill(0)
-        this.p.rect(
-          this.x,
-          this.y,
-          this.displayDimensions,
-          this.displayDimensions
-        )
+        // Untergetauchter Teil des Würfels berechnen
+        if (this.y > liquid.y) {
+          this.submergedPart = 1
+          this.submergedVolume = this.volume * this.submergedPart
+        } else if (this.y + this.dim < liquid.y) {
+          this.submergedPart = 0
+          this.submergedVolume = 0
+        } else {
+          this.submergedPart = (this.y + this.dim - liquid.y) / this.dim
+          this.submergedVolume = this.volume * this.submergedPart
+        }
       } else {
         this.update()
-        this.p.fill(0)
-        this.p.rect(
-          this.x,
-          this.y,
-          this.displayDimensions,
-          this.displayDimensions
-        )
       }
+      this.p.fill(0)
+      this.p.square(this.x * scale, this.y * scale, this.dim * scale)
     }
   }
 
   class Liquid {
     constructor(p) {
       this.p = p
-      this.density = 1000
-      this.startY = 300
+      this.width = this.p.width / scale
+      this.height = this.p.height / scale
+      this.density = startingLiquidDensity
+      this.startY = 300 / scale
       this.y = this.startY
     }
 
     update() {
-      this.density = liquidDensitySlider.value()
-
-      this.y =
-        this.startY -
-        (cube.submergedPart * Math.pow(cube.displayDimensions, 2)) /
-          this.p.width
+      this.y = this.startY - (cube.submergedPart * cube.dim ** 2) / this.width
     }
 
     display() {
       this.update()
       this.p.fill('#607d84')
-      this.p.rect(0, this.y, this.p.width, this.p.height)
+      this.p.rect(0, this.y * scale, this.p.width, this.p.height)
     }
   }
   return (
