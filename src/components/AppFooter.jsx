5546bbf8060c0d94c180d66b75785176e44c9132
PerOle15 <ole.pechmann@gmail.com>
2022-10-17 17:00:27 +0200

    footer text

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -2,7 +2,7 @@ function AppFooter() {
   return (
     <footer className='main-footer'>
       <div className='container'>
-        <p>Copyright &copy; 2022</p>
+        <p>Copyright &copy; 2022; Evgin & von Pechmann</p>
       </div>
     </footer>
   )
