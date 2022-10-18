96f6ea05aaa59609c9312376e7779d2c4bb5fd65
PerOle15 <ole.pechmann@gmail.com>
2022-10-18 20:51:11 +0200

    pendulum revised

--- src/sketches/PendulumSketch.jsx
+++ src/sketches/PendulumSketch.jsx
@@ -4,7 +4,12 @@ import React from 'react'
 function PendulumSketch() {
   const frames = 60
   const scale = 100
-  const localGravity = 9.81
+  const g = 9.81
+
+  const cWidth = 800
+  const cHeight = 500
+
+  const width = cWidth / scale
 
   const minLength = 0.5
   const maxLength = 3.5
@@ -23,7 +28,7 @@ function PendulumSketch() {
   let pendulum
 
   const setup = (p, canvasParentRef) => {
-    p.createCanvas(800, 500).parent(canvasParentRef)
+    p.createCanvas(cWidth, cHeight).parent(canvasParentRef)
     p.frameRate(frames)
     p.background(180)
 
@@ -75,7 +80,6 @@ function PendulumSketch() {
       .input(() => {
         lengthLabel.html(`Pendellänge: ${lengthSlider.value().toFixed(2)} m`)
         pendulum.length = lengthSlider.value()
-        pendulum.displayLength = pendulum.length * scale
         setAngle()
       })
     lengthLabel.html(`Pendellänge: ${lengthSlider.value().toFixed(2)} m`)
@@ -92,8 +96,7 @@ function PendulumSketch() {
       .input(() => {
         massLabel.html(`Pendelmasse: ${massSlider.value().toFixed(2)} kg`)
         pendulum.mass = massSlider.value()
-        pendulum.radius =
-          (Math.pow((pendulum.mass * 3) / 4 / Math.PI, 1 / 3) / 3) * scale
+        pendulum.calcRadius()
       })
     massLabel.html(`Pendelmasse: ${massSlider.value().toFixed(2)} kg`)
   }
@@ -115,34 +118,39 @@ function PendulumSketch() {
       this.p = p
       this.mass = 1
       this.length = startingLength
-      this.displayLength = this.length * scale
-      this.radius = (Math.pow((this.mass * 3) / 4 / Math.PI, 1 / 3) / 3) * scale
+      this.calcRadius()
       this.angle = Math.PI / 3
-      this.originX = this.p.width / 2
+      this.originX = width / 2
       this.angleVelocity = 0
       this.angleAcceleration = 0
     }
 
+    calcRadius() {
+      this.radius = Math.pow((this.mass * 3) / 4 / Math.PI, 1 / 3) / 3
+    }
     calcPosition() {
-      this.x = Math.sin(this.angle) * this.displayLength + this.originX
-      this.y = Math.cos(this.angle) * this.displayLength
+      this.x = Math.sin(this.angle) * this.length + this.originX
+      this.y = Math.cos(this.angle) * this.length
     }
 
     update() {
-      this.resultingForce = Math.sin(this.angle) * localGravity
-      this.angleAcceleration = -this.resultingForce / this.length / frames ** 2
+      this.resultingForce = Math.sin(this.angle) * g
+      this.angleAcceleration = -this.resultingForce / this.length / frames
       this.angleVelocity += this.angleAcceleration
-      this.angle += this.angleVelocity
+      this.angle += this.angleVelocity / frames
 
       this.calcPosition()
+
+      // this.force = this.mass * g * Math.sin(this.angle)
+      // this.acc = this.force / this.mass / frames
     }
 
     display() {
       this.update()
       this.p.stroke(0)
       this.p.fill(0)
-      this.p.line(this.p.width / 2, 0, this.x, this.y)
-      this.p.circle(this.x, this.y, this.radius)
+      this.p.line(cWidth / 2, 0, this.x * scale, this.y * scale)
+      this.p.circle(this.x * scale, this.y * scale, this.radius * scale)
     }
   }
 
--- src/studyPages/PendulumPage.jsx
+++ src/studyPages/PendulumPage.jsx
@@ -1,17 +1,25 @@
 import PendulumSketch from '../sketches/PendulumSketch'
 import ImportantFormulas from '../components/ImportantFormulas'
 import M from '../components/Maths'
+import L from '../components/PageLink'
 
 function PendulumPage() {
   document.title = 'Fysihka - Pendel'
   const formulas = []
+  const fr = '$$F_{R}=m\\cdot g\\cdot sin(\\alpha)$$'
+  formulas.push({ title: 'Rückstellkraft', formula: fr })
   const oscillationPeriod = '$$T=2\\cdot\\pi\\cdot\\sqrt{\\frac{l}{g}}$$'
-  formulas.push({ title: 'Schwingungsdauer', formula: oscillationPeriod })
+  formulas.push({
+    title: 'Schwingungsdauer für Anfangswinkel < 10°',
+    formula: oscillationPeriod,
+  })
   const pendulumLength = '$$l=\\frac{T^2\\cdot g}{4\\cdot\\pi^2}$$'
   formulas.push({
     title: 'Pendellänge (gleiche Formel, nur umgeformt)',
     formula: pendulumLength,
   })
+
+  const force = 'F_{R}'
   return (
     <div className='container'>
       <p className='page-heading'>Pendel</p>
@@ -19,14 +27,11 @@ function PendulumPage() {
       <section className='topic-description'>
         <div className='short-description'>
           <p>
-            Ein Pendel ist eines der einfachsten physikalischen Objekte.
             Wahrscheinlich kennst du Pendel bereits aus alten Standuhren, die
             hin und her Schwingen und so die Zeit angeben. Grundsätzlich
             versteht man unter einem Pendel einen Körper, der drehbar aufgehängt
             ist. Die einfachste Art eines solchen Pendels ist ein Fadenpendel,
-            also ein Körper, der mithilfe eines Fadens aufgehängt ist. Die
-            Einfachheit eines Pendels kommt daher, dass dessen Schwingungsdauer
-            nur von der Länge der Schnur und der Gravitation abhängt.
+            also ein Körper, der mithilfe eines Fadens aufgehängt ist.
           </p>
         </div>
         <ImportantFormulas content={formulas} />
@@ -35,20 +40,28 @@ function PendulumPage() {
           <p>
             In der Animation oben wird das Pendel vereinfacht dargestellt. Es
             handelt sich dabei um ein völlig starres Pendel, das ohne
-            Luftwiderstand schwingt.
+            Luftwiderstand und Reibung schwingt. Ausserdem gilt die Berechnung
+            der Schwingungsdauer nur annähernd und nur für maximale
+            Auslenkungswinkel &lt; 10°. Je grösser der Auslenkungswinkel des
+            Pendels, desto ungenauer wird die Berechnung der Schwingungsdauer.
+            Die exakte Berechnung der Schwingungsdauer eines Pendels ist viel
+            komplexer.
             <br />
-            Wenn man von diesen Bedingungen ausgeht, lässt sich die
-            Schwingungsdauer mit einer einfachen Formel berechnen. Als
-            Schwingungsdauer wird die Zeit bezeichnet, in der das Pendel eine
-            gesamte Schwingung, also einmal hin und her, durchläuft. In der
+            Als Schwingungsdauer wird die Zeit bezeichnet, in der das Pendel
+            eine gesamte Schwingung, also einmal hin und her, durchläuft. In der
             Formel steht <M>T</M> für die Schwingungsdauer, <M>l</M> für die
             Länge des Pendels und <M>g</M> für den Ortsfaktor, also die
-            Gravitation. Um die Länge des Pendels bei gegebener Schwingungsdauer
-            herauszufinden, muss man den Term lediglich nach <M>l</M> auflösen.
-            Wenn wir nun eine Pendeluhr bauen wollen, die jede Sekunde einmal
-            tickt, das heisst für eine Hin- und Herbewegung braucht es 2
-            Sekunden <M>(T=2s)</M>, können wir dafür die benötigte Pendellänge
-            berechnen. Man kommt auf eine Länge von ca. 99.4cm.
+            Gravitationsstärke. Um die Länge des Pendels bei gegebener
+            Schwingungsdauer herauszufinden, muss man den Term lediglich nach{' '}
+            <M>l</M> auflösen. Wenn wir nun eine Pendeluhr bauen wollen, die
+            jede Sekunde einmal tickt, das heisst für eine Hin- und Herbewegung
+            braucht es 2 Sekunden <M>(T=2s)</M>, können wir dafür die benötigte
+            Pendellänge berechnen. Man kommt auf eine Länge von ca. 99.4cm.
+            <br />
+            Die treibende Kraft für die Bewegung des Pendels ist die
+            Rückstellkraft <M>{force}</M>. Sie ist der Teil der{' '}
+            <L to='mass'>Gewichtskraft</L>, der senkrecht zur Pendelschnur
+            wirkt.
           </p>
         </div>
       </section>
