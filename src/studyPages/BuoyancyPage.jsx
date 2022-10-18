6f384e32f54a16bdadffe42543658eaf74d4894b
PerOle15 <ole.pechmann@gmail.com>
2022-10-18 21:08:59 +0200

    buoyancy desc revised

--- src/studyPages/BuoyancyPage.jsx
+++ src/studyPages/BuoyancyPage.jsx
@@ -1,6 +1,7 @@
 import BuoyancySketch from '../sketches/BuoyancySketch'
 import ImportantFormulas from '../components/ImportantFormulas'
 import M from '../components/Maths'
+import L from '../components/PageLink'
 
 function BuoyancyPage() {
   document.title = 'Fysihka - Auftrieb'
@@ -39,29 +40,31 @@ function BuoyancyPage() {
         <br />
         <div className='long-description'>
           <p>
-            Die Auftriebskraft entspricht der Dichte der Flüssigkeit (
-            <M>{densityLiquid}</M>) multipliziert mit dem eingetauchten Volumen
-            des Körpers <M>{volIn}</M> und dem Ortsfaktor <M>g</M>. Was man hier
-            berechnet ist die gleiche Kraft wie die Gewichtskraft der vom
-            eingetauchten Körper verdrängten Flüssigkeit. Das Volumen des
-            eingetauchten Teils des Körpers ist gleich dem Volumen der
-            verdrängten Flüssigkeit, also ergibt <M>{volumeLiquid}</M> die Masse{' '}
-            <M>m</M> der verdrängten Flüssigkeit. Das Prinzip der Auftriebskraft
-            ist folgendermassen zu verstehen. Wenn ein Körper in eine
-            Flüssigkeit - oder in ein Gas - eingetaucht wird, will die
-            Flüssigkeit züruck an diesen Platz, wo sich jetzt der Körper
-            befindet. Die Flüssigkeit drückt also genau mit der Schwerkraft, die
-            die verdrängte Flüssigkeit bewirkt, auf das Objekt. Ohne Schwerkraft
-            gäbe es also keinen Auftrieb. Wird beispielsweise ein Wasserball im
-            All, fern von allen Planeten und Sternen, in Wasser getaucht, so
-            gibt es diesen Effekt nicht, der Ball wird nicht aus dem Wasser
-            herausgedrückt.
+            Die Auftriebskraft entspricht der <L to='density'>Dichte</L> der
+            Flüssigkeit (<M>{densityLiquid}</M>) multipliziert mit dem
+            eingetauchten Volumen des Körpers <M>{volIn}</M> und dem Ortsfaktor{' '}
+            <M>g</M>. Was man hier berechnet, ist die gleiche Kraft, wie die
+            Gewichtskraft der vom eingetauchten Körper verdrängten Flüssigkeit.
+            Das Volumen des eingetauchten Teils des Körpers ist gleich dem
+            Volumen der verdrängten Flüssigkeit, also ergibt{' '}
+            <M>{volumeLiquid}</M> die Masse <M>m</M> der verdrängten
+            Flüssigkeit.
+            <br />
+            Das Prinzip der Auftriebskraft ist folgendermassen zu verstehen.
+            Wenn ein Objekt in eine Flüssigkeit - oder in ein Gas - eingetaucht
+            wird, will die Flüssigkeit züruck an diesen Platz, wo sich jetzt das
+            Objekt befindet. Die Flüssigkeit drückt also genau mit der
+            Schwerkraft, die die verdrängte Flüssigkeit bewirkt, auf das Objekt.
+            Ohne Schwerkraft gäbe es also keinen Auftrieb. Wird beispielsweise
+            ein Wasserball im All, fern von allen Planeten und Sternen, in
+            Wasser getaucht, so gibt es den Effekt des Auftriebs nicht, der Ball
+            wird nicht aus wieder aus dem Wasser herausspicken.
             <br />
             Die zweite Formel gibt an, welcher Teil eines Körpers in eine
-            Flüssigkeit eingtaucht und welcher herausschaut, sobald der Körper
-            sich nicht mehr bewegt. Nehmen wir an, wir tauchen einen Gegenstand
-            mit einer Dichte von 750kg/m<sup>3</sup> in Wasser, mit ungefähr
-            1000kg/m<sup>3</sup>. Dann ist das Verhältnis aus Dichte des
+            Flüssigkeit eintaucht und welcher herausschaut, sobald der Körper
+            sich beruhigt hat und nicht mehr bewegt. Nehmen wir an, wir tauchen
+            einen Gegenstand mit einer Dichte von <M>750kg/m^3</M> in Wasser{' '}
+            <M>\rho = 1000kg/m^3</M>. Dann ist das Verhältnis aus Dichte des
             Gegenstands und Dichte des Wassers 0.75. Die Dichte des Gegenstands
             beträgt also 75% der Dichte des Wassers. Daraus lässt sich
             schliessen, dass 75% des Körpers eingetaucht sind, wenn er ins
