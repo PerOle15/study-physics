f7f1b340d6de0c6c6cd5e91bd90c1bd04929c55b
PerOle15 <ole.pechmann@gmail.com>
2022-10-18 21:40:04 +0200

    acc desc revised

--- src/studyPages/AccelerationPage.jsx
+++ src/studyPages/AccelerationPage.jsx
@@ -7,8 +7,11 @@ function AccelerationPage() {
   const formulas = []
   const vel = '$$v=\\frac{\\Delta x}{\\Delta t}$$'
   const acc = '$$a=\\frac{\\Delta v}{\\Delta t}$$'
+  const force = '$$F=m\\cdot a$$'
   formulas.push({ title: 'Geschwindigkeit', formula: vel })
   formulas.push({ title: 'Beschleunigung', formula: acc })
+  formulas.push({ title: '2. Newtonsches Bewegungsgesetz', formula: force })
+
   return (
     <div className='container'>
       <p className='page-heading'>AccelerationPage</p>
@@ -16,15 +19,15 @@ function AccelerationPage() {
       <section className='topic-description'>
         <div className='short-description'>
           <p>
-            Die Beschleunigung ist die Änderungsrate der Geschwindigkeit im
-            Verhältnis zur Zeit. Sie ist eine Vektorgröße (d.h. sie hat eine
-            Richtung und einen Betrag) und wird in Metern pro Sekunde zum
-            Quadrat (<M>m/s^2</M>) gemessen. Die Beschleunigung kann durch eine
-            Vielzahl von Kräften verursacht werden, z. B. durch Schwerkraft,
-            Reibung oder sogar Luftwiderstand. Eine Beschleunigung tritt immer
-            dann auf, wenn sich die Geschwindigkeit ändert. Diese Änderung kann
-            in Form einer Zunahme der Geschwindigkeit, einer Abnahme der
-            Geschwindigkeit oder einer Richtungsänderung erfolgen.
+            Die Beschleunigung ist die Änderungsrate der Geschwindigkeit. Sie
+            ist eine Vektorgröße (d.h. sie hat eine Richtung und einen Betrag)
+            und wird in Metern pro Sekunde zum Quadrat (<M>m/s^2</M>) gemessen.
+            Die Beschleunigung kann durch eine Vielzahl von Kräften verursacht
+            werden, z. B. durch Schwerkraft, Reibung oder sogar Luftwiderstand.
+            Eine Beschleunigung tritt immer dann auf, wenn sich die
+            Geschwindigkeit ändert. Diese Änderung kann in Form einer Zunahme
+            der Geschwindigkeit, einer Abnahme der Geschwindigkeit oder einer
+            Richtungsänderung erfolgen.
           </p>
         </div>
         <ImportantFormulas content={formulas} />
@@ -32,39 +35,42 @@ function AccelerationPage() {
         <div className='long-description'>
           <p>
             Es gibt verschiedene Arten der Beschleunigung: z.B. lineare,
-            winkelförmige und radiale Beschleunigung. (Winkelbeschleunigung ist
+            winkelförmige und radiale Beschleunigung. Winkelbeschleunigung ist
             die Beschleunigung in einem rotierenden Körper und
             Radialbeschleunigung die Beschleunigung auf einen zentralen Punkt zu
-            oder von ihm weg).
+            oder von ihm weg.
             <br />
             In unserer Simulation seht ihr eine gleichmässige, lineare
             Beschleunigung. Kennzeichnend dafür ist die gerade Strecke, auf der
             das Objekt sich bewegt (also linear) und natürlich die konstante
             Beschleunigung (gleichmässige Beschleunigung).
             <br />
-            Da a = konst. ist, verändert sich die Geschwindigkeit v
+            Da <M>a</M> konstant ist, verändert sich die Geschwindigkeit{' '}
+            <M>v</M>
             gleichmässig, welches ihr am blauen Graphen beobachten könnt. Die
             Sprünge des blauen Graphen sind zurückzuführen auf den
-            Vorzeichenwechsel der Geschwindigkeit, welche auch eine Richtung
-            besitzt, damit die Simulation durgehend laufen kann.
+            Vorzeichenwechsel der Geschwindigkeit Beim Aufprall auf die Wand.
+            Die Richtung der Geschwindigkeit wird angegeben durch das
+            Vorzeichen.
             <br />
-            Wenn ihr nun die Beschleunigung so ändert, dass sie negativ ist,
-            dann werdet ihr merken, dass das Objekt seine Fahrtrichtung kehrt
-            (Beschleunigung ist eine Vektorgrösse!). Damit seht ihr, dass eine
-            negative Beschleunigung gar nicht abbremst, sondern die Richtung
-            ändert, in welche das Objekt beschleunigt.
+            Wenn man nun die Beschleunigung so ändert, dass sie negativ ist,
+            dann wird man merken, dass das Objekt seine Fahrtrichtung kehrt
+            (Beschleunigung ist eine Vektorgrösse!). Eine negative
+            Beschleunigung heisst nicht zwingend, dass das betroffene Objekt
+            abbremst. Es heisst nur, dass das Objekt in die entgegengesetzte
+            Richtung beschleunigt wird.
             <br />
-            Die Beschleunigung wird sehr oft durch eine Kraft verursacht. Je
-            größer die Kraft, desto größer ist die Beschleunigung. Auch die
-            Masse eines Objekts wirkt sich auf die Beschleunigung aus. Je größer
-            die Masse ist, desto geringer ist die Beschleunigung.
+            Die Beschleunigung wird oft durch eine Kraft verursacht. Je größer
+            die Kraft, desto größer ist die Beschleunigung. Auch die Masse eines
+            Objekts wirkt sich auf die Beschleunigung aus. Je größer die Masse
+            ist, desto geringer ist die Beschleunigung.
             <br />
             Newtons zweites Bewegungsgesetz besagt, dass die Beschleunigung
-            eines Objekts proportional zu der auf die wirkende Kraft und
+            eines Objekts proportional zu der auf das Objekt wirkenden Kraft und
             umgekehrt proportional zu seiner Masse ist. Das heißt, wenn man die
             Kraft verdoppelt, verdoppelt sich auch die Beschleunigung.
             Verdoppelt man jedoch die Masse, so halbiert sich die
-            Beschleunigung. (noch nicht in unserer Simulation integriert)
+            Beschleunigung.
           </p>
         </div>
       </section>
